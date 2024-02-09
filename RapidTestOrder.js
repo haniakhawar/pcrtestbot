93f3c52721a27b8f363a5e531498b0bb288a031b
haniakhawar <khaw1550@mylaurier.ca>
2024-02-08 21:24:12 -0500

    add size

--- RapidTestOrder.js
+++ RapidTestOrder.js
@@ -3,19 +3,26 @@ class RapidTestOrder {
     this.OrderState = {
       WELCOMING: () => {
         let aReturn = [];
-        this.stateCur = this.OrderState.RESERVING;
+        this.stateCur = this.OrderState.SIZE;
         aReturn.push("Welcome to Hania's Kitchen");
         aReturn.push("Would size would like to place an order of our famous burger meal?");
+        return aReturn;
+      },
+      SIZE: () =>{
+        let aReturn = [];
+        this.stateCur = this.OrderState.RESERVING;
+        aReturn.push("What toppings would you like?");
+        aReturn.push("We have Lettuce, Tomatoes, Onions, and Pickles");
         return aReturn;
       },
       RESERVING: (sInput) => {
         let aReturn = [];
         this.isDone = true;
         if (sInput.toLowerCase().startsWith('y')) {
-          aReturn.push(`Your rapid test is reserved under the phone number ${this.sFrom}`);
+          aReturn.push(`Your burger is reserved under the phone number ${this.sFrom}`);
           let d = new Date();
           d.setMinutes(d.getMinutes() + 120);
-          aReturn.push(`Please pick it up at 123 Tidy St., Acton before ${d.toTimeString()}`);
+          aReturn.push(`Please pick it up at 123 Colbourne St., Brantford before ${d.toTimeString()}`);
         } else {
           aReturn.push("Thanks for trying our reservation system");
           aReturn.push("Maybe next time")
